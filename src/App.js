254449d7ff605c57ed7aebd4a34e7b83fee605c4
nehakoli <nehakoli78@gmail.com>
2022-08-06 13:34:45 +0530

    Uploading project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from "./Components/SearchBar";
+import BookData from "./Data.json";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar placeholder="Enter a Book Name..." data={BookData}/>
+    </div>
+  );
+}
+
+export default App;
+ 
